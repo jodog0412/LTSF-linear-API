e2beac81e30c0381d23f7d6ffa214d5f7bd60b8a
Hyunsung <jodog0412@gmail.com>
2024-01-15 22:36:35 +0900

    [style] revise annotation

--- ex_mobility.py
+++ ex_mobility.py
@@ -1,16 +1,25 @@
 from function import *
 import pandas as pd
+
 window_size, forecast_size = 24*7,24
 raw=pd.read_csv('./data/서인천IC-부평IC 평균속도.csv',encoding='CP949').set_index('집계일시').drop('Unnamed: 0',axis=1)
 plt.plot(raw)
 plt.show()
-date, data=split_data(raw,0,index=True) # preprocess raw data
+
+''' 1. preprocess raw data '''
+date, data=split_data(raw,0,index=True) 
+
+''' 2. build dataloader '''
 dataloader=build_dataLoader(data,
                             window_size=window_size,
                             forecast_size=forecast_size,
-                            batch_size=4) #make dataloader
+                            batch_size=4) 
+
+''' 3. train and evaluate '''
 pred=trainer(data,
              dataloader,
              window_size=window_size,
-             forecast_size=forecast_size).implement() #train and evaluate
-figureplot(date,data,pred,window_size,forecast_size) #plot the result.
+             forecast_size=forecast_size).implement() 
+
+''' 4. plot the result ''' 
+figureplot(date,data,pred,window_size,forecast_size) 
--- ex_stock.py
+++ ex_stock.py
@@ -1,20 +1,20 @@
-
 from function import *
 import pandas as pd
-
-#Usage example 1
 from yahooquery import Ticker
+
 raw=Ticker('AAPL').history(period='1y').xs('AAPL')
-window_size,forecast_size=15,5
-date, data=split_data(raw,'adjclose') # preprocess raw data
+window_size,forecast_size=30,10
+''' 1. preprocess raw data '''
+date, data=split_data(raw,'adjclose')
+''' 2. build dataloader '''
 dataloader=build_dataLoader(data,
                             window_size=window_size,
                             forecast_size=forecast_size,
-                            batch_size=4) #make dataloader
+                            batch_size=4)
+''' 3. train and evaluate '''
 pred=trainer(data,
              dataloader,
              window_size=window_size,
-             forecast_size=forecast_size).implement() #train and evaluate
-figureplot(date,data,pred,window_size,forecast_size) #plot the result.
-
-
+             forecast_size=forecast_size).implement() 
+''' 4. plot the result '''
+figureplot(date,data,pred,window_size,forecast_size)
